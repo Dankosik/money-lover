93770ace393b7386a5412072e91f71abb567ee67
dankos <daniil.koryto@gmail.com>
2021-09-19 03:16:19 +1000

    added custom methods to repo

--- src/main/java/ru/dankos/moneylover/repository/OperationRepository.java
+++ src/main/java/ru/dankos/moneylover/repository/OperationRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.dankos.moneylover.domain.Operation;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, Long> {
+    List<Operation> findOperationByDateBetween(Date date, Date date2);
 }
